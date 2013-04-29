de41d56ba5b41f3bce18dcd6fee7892267ffb7ce
XhmikosR <XhmikosR@1b9aca29-3beb-4759-8d12-af905a04d467>
2013-04-29 18:10:42 +0000

    update copyright year for SVN.h

--- src/SVN.h
+++ src/SVN.h
@@ -1,6 +1,6 @@
 // SVNProtocolHandler - an asynchronous protocol handler for the svn:// protocol
 
-// Copyright (C) 2008, 2011-2012 - Stefan Kueng
+// Copyright (C) 2008, 2011-2013 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
