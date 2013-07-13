ad47e7d8be8d41ebce209263dba4aefe20e947f0
XhmikosR <XhmikosR@1b9aca29-3beb-4759-8d12-af905a04d467>
2013-07-13 08:05:36 +0000

    Add a simple test script.

---
+++ test/test.bat
@@ -0,0 +1,56 @@
+@echo off
+setlocal
+
+set REPOROOT=file:///C:/_test_repo
+set REPOPATH=C:\_test_repo
+set WC=C:\_test_wc
+
+:: create empty repos & working copy
+
+if exist %REPOPATH% rd /s /q %REPOPATH%
+svnadmin create %REPOPATH%
+
+if exist %WC% rd /s /q %WC%
+svn co %REPOROOT% %WC%
+
+:: r1: initial content
+
+mkdir %WC%\trunk
+mkdir %WC%\branches
+mkdir %WC%\tags
+
+echo "foo" > %WC%\trunk\foo.txt
+echo "bar" > %WC%\trunk\bar.txt
+echo "baz" > %WC%\trunk\baz.txt
+
+svn add %WC%\trunk
+svn add %WC%\branches
+svn add %WC%\tags
+
+svn ci %WC% -m "initial content"
+
+:: r2: create branch
+
+svn cp %WC%\trunk %WC%\branches\b
+svn ci %WC%\branches -m "open branch"
+
+:: r3: modify all files on trunk
+
+echo "new foo" > %WC%\trunk\foo.txt
+echo "new bar" > %WC%\trunk\bar.txt
+echo "new baz" > %WC%\trunk\baz.txt
+
+svn ci %WC% -m "modify all files on trunk"
+
+:: r6: tagging
+
+svn cp %WC%\branches\b %WC%\tags\t
+svn ci %WC%\tags -m "tagging current b content"
+
+echo Starting svnserve...
+echo Open IE in svn://localhost:3690/
+
+svnserve.exe --daemon --root %REPOPATH% --listen-host localhost
+
+endlocal
+pause
